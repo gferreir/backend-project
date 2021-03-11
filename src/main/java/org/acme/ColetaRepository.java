e171e9cf8256c972d19ed09d13b58d1c79f586cd
gferreir <guilherme.nmf@gmail.com>
2021-03-11 17:28:10 -0300

    feature: Implementação ColetaRepository com a lógica de busca para substituir que era feito em ColetaResouce

---
+++ src/main/java/org/acme/ColetaRepository.java
@@ -0,0 +1,68 @@
+package org.acme;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.NotAcceptableException;
+import javax.ws.rs.NotFoundException;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+@ApplicationScoped
+public class ColetaRepository implements PanacheRepository<Coleta>{
+
+	public List<Coleta>buscaTodasColetas(){
+		return Coleta.listAll();
+	}
+
+	public Coleta buscaColetaPorCodigo(String codigo){
+		try {
+			return Coleta.find("codigoColeta", codigo).firstResult();
+		}catch (Exception e) {
+			throw new NotFoundException();
+		}
+	}
+
+	public void cadastraColeta(CadastrarColetaDTO dto){
+		try {
+			Coleta c = new Coleta();
+			c.codigoColeta = dto.codigoColeta;
+			c.horaInicio = dto.horaInicio;
+			c.diasSemana = dto.diasSemana;
+			c.tipoColeta = dto.tipoColeta;
+			c.rota = dto.rota;
+			c.persist();
+		} catch (Exception e) {
+			throw new NotAcceptableException();
+		}
+	}
+
+	public void atualizaColeta1(String codigo, CadastrarColetaDTO dto){
+		try{
+			Coleta c = Coleta.find("codigoColeta", codigo).firstResult();
+			Optional<Coleta> coletaOp = Coleta.findByIdOptional(c.id);
+			
+			Coleta coleta = coletaOp.get();
+			coleta.codigoColeta = dto.codigoColeta;
+			coleta.horaInicio = dto.horaInicio;
+			coleta.diasSemana = dto.diasSemana;
+			coleta.tipoColeta = dto.tipoColeta;
+			coleta.rota = dto.rota;
+			coleta.persist();
+		}catch (Exception e) {
+			throw new NotFoundException();
+		}
+	}
+
+	public void deletaColeta(String codigo){
+		Coleta c = Coleta.find("codigoColeta", codigo).firstResult();
+
+		try {
+			c.delete();
+		} catch (Exception e) {
+			throw new NotFoundException();
+		}
+	}
+
+}
--- src/main/java/org/acme/ColetaResource.java
+++ src/main/java/org/acme/ColetaResource.java
@@ -1,11 +1,10 @@
 package org.acme;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotAcceptableException;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -14,75 +13,44 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import java.util.List;
-import java.util.Optional;
 
 @Path("coleta")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ColetaResource {
 
+	@Inject
+	ColetaRepository coletaRepository;
+
 	@GET
 	public List<Coleta>buscaTodasColetas(){
-		return Coleta.listAll();
+		return coletaRepository.buscaTodasColetas();
 	}
 
 	@GET
 	@Path("{codigo}")
 	public Coleta buscaColetaPorCodigo(@PathParam("codigo") String codigo){
-		try {
-			return Coleta.find("codigoColeta", codigo).firstResult();
-		}catch (Exception e) {
-			throw new NotFoundException();
-		}
+		return coletaRepository.buscaColetaPorCodigo(codigo);
 	}
 
 	@POST
 	@Transactional
 	public void cadastraColeta(CadastrarColetaDTO dto){
-		try {
-			Coleta c = new Coleta();
-			c.codigoColeta = dto.codigoColeta;
-			c.horaInicio = dto.horaInicio;
-			c.diasSemana = dto.diasSemana;
-			c.tipoColeta = dto.tipoColeta;
-			c.rota = dto.rota;
-			c.persist();
-		} catch (Exception e) {
-			throw new NotAcceptableException();
-		}
+		coletaRepository.cadastraColeta(dto);
 	}
 
 	@PUT
 	@Transactional
 	@Path("{codigo}")
 	public void atualizaColeta1(@PathParam("codigo") String codigo, CadastrarColetaDTO dto){
-		try{
-			Coleta c = Coleta.find("codigoColeta", codigo).firstResult();
-			Optional<Coleta> coletaOp = Coleta.findByIdOptional(c.id);
-			
-			Coleta coleta = coletaOp.get();
-			coleta.codigoColeta = dto.codigoColeta;
-			coleta.horaInicio = dto.horaInicio;
-			coleta.diasSemana = dto.diasSemana;
-			coleta.tipoColeta = dto.tipoColeta;
-			coleta.rota = dto.rota;
-			coleta.persist();
-		}catch (Exception e) {
-			throw new NotFoundException();
-		}
+		coletaRepository.atualizaColeta1(codigo, dto);
 	}
 
 	@DELETE
 	@Transactional
 	@Path("{codigo}")
 	public void deletaColeta(@PathParam("codigo") String codigo){
-		Coleta c = Coleta.find("codigoColeta", codigo).firstResult();
-
-		try {
-			c.delete();
-		} catch (Exception e) {
-			throw new NotFoundException();
-		}
+		coletaRepository.deletaColeta(codigo);
 	}
 
 }
